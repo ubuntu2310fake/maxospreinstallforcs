67c169925232cdc367fd0da599b8f7ef81a53435
ubuntu2310fake <152691685+ubuntu2310fake@users.noreply.github.com>
2024-01-13 21:56:05 +0700

    Update createmaxos.sh

--- createmaxos.sh
+++ createmaxos.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "downloading max os"
-sudo wget -O /tmp/maxos.qcow2 https://app.vagrantup.com/linuxusersfake/boxes/macos/versions/1.0/providers/maxos/amd64/vagrant.box
+sudo wget -O /tmp/mac_hdd_ng.img https://app.vagrantup.com/linuxusersfake/boxes/macos/versions/1.0/providers/maxos/amd64/vagrant.box
